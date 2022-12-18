7786607536dc056fdf60a80c926adf6e1b523431
Hector Franco T <42476070+UnPolinomio@users.noreply.github.com>
2022-12-18 13:27:13 -0600

    Initial commit

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define _                           \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);                       \
+  cout.tie(0)
+#define deb(x) cout << #x << ": " << (x) << '\n';
+#define all(x) begin(x), end(x)
+using lli = long long int;
+using vi = vector<lli>;
+using vb = vector<bool>;
+using ii = pair<lli, lli>;
+
+int main() {
+  _;
+
+  return EXIT_SUCCESS;
+}
